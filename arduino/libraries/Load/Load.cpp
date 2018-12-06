92b7f8224cfc9718d98b006b1d1e7411a93169bf
Francisco <francisco.aurindo@gmail.com>
2018-12-06 23:57:29 +0000

    Added Load functions and HX711 library

--- arduino/libraries/Load/Load.cpp
+++ arduino/libraries/Load/Load.cpp
@@ -1,9 +1,15 @@
 #include "Arduino.h"
 #include "Load.h"
+#include "HX711.h"
 
 Load::Load(int loadPin){
+	HX711 scale(DOUT, CLK);
+	scale.set_scale(-2006490); // calibration factor: -2006490
     }
 
 float Load::getVolume(){
-    return 0;
+	scale.power_up();
+	float weight = scale.get_units(3)
+	scale.power_down();
+    return weight ;
 }
